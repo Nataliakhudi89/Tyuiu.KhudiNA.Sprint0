3562304e7e3c08e3a378e3917fd7deb0c3361198
Nataliakhudi89 <nataliakhudi@mail.ru>
2024-09-18 21:12:46 +0500

    Task1

--- Tyuiu.KhudiNA.Sprint0.Task0.V0/Program.cs
+++ Tyuiu.KhudiNA.Sprint0.Task0.V0/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Text;Е
 using System.Threading.Tasks;
 
 namespace Tyuiu.KhudiNA.Sprint0.Task0.V0
---
+++ Tyuiu.KhudiNA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.KhudiNA.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Htllo, World");
+            string name = "Наталья";
+            int age = 20;
+            Console.WriteLine("Здравствйте меня зовут " + name + " мне " + age + " лет!");
+            Console.ReadKey("")
+        }
+    }
+}

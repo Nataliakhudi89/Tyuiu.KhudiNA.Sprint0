9f09a36def86077da57c4b6d58c6da8b672eb843
Nataliakhudi89 <nataliakhudi@mail.ru>
2024-09-19 02:46:42 +0500

    Task2

---
+++ Tyuiu.KhudiNA.Sprint0.Task2.V0.Lib/DataService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.KhudiNA.Sprint0.Task2.V0.Lib
+{
+    public class Class1
+    {
+        public static string GetMessage(string name)
+
+            return $"Привет, {name}";
+    }
+}
---
+++ Tyuiu.KhudiNA.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.KhudiNA.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.KhudiNA.Sprint0.Task2.V0
+{
+    internal class Program
+    {
+        private static object DataService;
+
+        static void Main(string[] args)
+        {
+            //Вызов класса DataService и метода GetMessage
+            //из библиотеки Tyuiu.KhudiNA.Sprint0.Task2.V0.Lib
+            Console.WriteLine(DataService.GetMessage("Наталья"));
+            Console.ReadKey();
+            Console.ReadKey();
+        }
+    }
+}
